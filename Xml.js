64422c41783f281030dab0b4ca7eaca2af77f6bb
kongsakda046 <kongsakda_001@hotmail.com>
2018-09-24 17:21:08 +0700

    top

---
+++ Xml.js
@@ -0,0 +1,13 @@
+
+///การทำานของ XML TO JSON
+var fs = require('fs'),
+    xml2js = require('xml2js');
+ 
+var parser = new xml2js.Parser();
+fs.readFile('XML/DecisionTree.xml', function(err, data) {
+    parser.parseString(data, function (err, result) {
+        console.dir(result);
+        
+        console.log('Read Finish');
+    });
+});
